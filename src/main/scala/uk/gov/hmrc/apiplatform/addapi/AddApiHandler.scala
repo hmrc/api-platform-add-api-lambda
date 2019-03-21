d1a27fda049d5d30b3a5168e3580b67beb878762
Chris Rowe <2837323+cjrowe@users.noreply.github.com>
2019-03-21 16:05:57 +0000

    API-3730: Handle different responses from AWS SDK

--- src/main/scala/uk/gov/hmrc/apiplatform/addapi/AddApiHandler.scala
+++ src/main/scala/uk/gov/hmrc/apiplatform/addapi/AddApiHandler.scala
@@ -1,6 +1,6 @@
 package uk.gov.hmrc.apiplatform.addapi
 
-import java.net.HttpURLConnection.HTTP_OK
+import java.net.HttpURLConnection.{HTTP_INTERNAL_ERROR, HTTP_OK}
 
 import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.events.{APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent}
@@ -11,6 +11,8 @@ import software.amazon.awssdk.core.SdkBytes
 import software.amazon.awssdk.services.apigateway.ApiGatewayClient
 import software.amazon.awssdk.services.apigateway.model.ImportRestApiRequest
 
+import scala.util.{Failure, Success, Try}
+
 
 class AddApiHandler(apiGatewayClient: ApiGatewayClient) extends Lambda[APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent] {
 
@@ -20,8 +22,13 @@ class AddApiHandler(apiGatewayClient: ApiGatewayClient) extends Lambda[APIGatewa
 
   override def handle(input: APIGatewayProxyRequestEvent, context: Context): Either[Nothing, APIGatewayProxyResponseEvent] = {
     val importApiRequest = ImportRestApiRequest.builder().body(SdkBytes.fromUtf8String(input.getBody)).build()
-    val apiGatewayResponse = apiGatewayClient.importRestApi(importApiRequest)
+    val apiGatewayResponse = Try(apiGatewayClient.importRestApi(importApiRequest))
 
-    Right(new APIGatewayProxyResponseEvent().withStatusCode(HTTP_OK).withBody(apiGatewayResponse.id()))
+    apiGatewayResponse match {
+      case Success(response) => Right(new APIGatewayProxyResponseEvent().withStatusCode(HTTP_OK).withBody(response.id()))
+      case Failure(exception) => Right(new APIGatewayProxyResponseEvent().withStatusCode(HTTP_INTERNAL_ERROR).withBody(exception.getMessage))
+    }
+
+
   }
 }
--- src/test/scala/uk/gov/hmrc/apiplatform/addapi/AddApiHandlerSpec.scala
+++ src/test/scala/uk/gov/hmrc/apiplatform/addapi/AddApiHandlerSpec.scala
@@ -1,34 +1,64 @@
 package uk.gov.hmrc.apiplatform.addapi
 
 import java.net.HttpURLConnection
+import java.net.HttpURLConnection.{HTTP_INTERNAL_ERROR, HTTP_OK}
 import java.util.UUID
 
 import com.amazonaws.services.lambda.runtime.events.{APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent}
 import com.amazonaws.services.lambda.runtime.Context
+import org.mockito.ArgumentCaptor
 import org.mockito.ArgumentMatchers.any
 import org.mockito.Mockito.when
 import org.scalatest._
 import org.scalatest.mockito.MockitoSugar
 import software.amazon.awssdk.services.apigateway.ApiGatewayClient
-import software.amazon.awssdk.services.apigateway.model.{ImportRestApiRequest, ImportRestApiResponse}
+import software.amazon.awssdk.services.apigateway.model.{ImportRestApiRequest, ImportRestApiResponse, UnauthorizedException}
 
-class AddApiHandlerSpec extends FlatSpec with Matchers with MockitoSugar {
+class AddApiHandlerSpec extends WordSpecLike with Matchers with MockitoSugar {
   trait Setup {
     val mockAPIGatewayClient = mock[ApiGatewayClient]
     val addApiHandler = new AddApiHandler(mockAPIGatewayClient)
   }
 
-  "The Add API handler" should "send API specification to AWS endpoint" in new Setup {
-    val id = UUID.randomUUID().toString
-    val apiGatewayResponse = ImportRestApiResponse.builder().id(id).build()
+  "The Add API handler" should {
+    "send API specification to AWS endpoint and return the created id" in new Setup {
+      val id = UUID.randomUUID().toString
+      val apiGatewayResponse = ImportRestApiResponse.builder().id(id).build()
 
-    when(mockAPIGatewayClient.importRestApi(any[ImportRestApiRequest])).thenReturn(apiGatewayResponse)
+      when(mockAPIGatewayClient.importRestApi(any[ImportRestApiRequest])).thenReturn(apiGatewayResponse)
 
-    val result: Either[Nothing, APIGatewayProxyResponseEvent] = addApiHandler.handle(new APIGatewayProxyRequestEvent().withBody("{}"), mock[Context])
+      val result: Either[Nothing, APIGatewayProxyResponseEvent] = addApiHandler.handle(new APIGatewayProxyRequestEvent().withBody("{}"), mock[Context])
 
-    result.isRight shouldBe true
-    val Right(responseEvent) = result
-    responseEvent.getStatusCode shouldEqual HttpURLConnection.HTTP_OK
-    responseEvent.getBody shouldEqual id
+      result.isRight shouldBe true
+      val Right(responseEvent) = result
+      responseEvent.getStatusCode shouldEqual HTTP_OK
+      responseEvent.getBody shouldEqual id
+    }
+
+    "correctly convert OpenAPI JSON into ImportRestApiRequest" in new Setup {
+      val apiGatewayResponse = ImportRestApiResponse.builder().id(UUID.randomUUID().toString).build()
+
+      val inputBody = "{foo: 'bar'}"
+
+      val importRestApiRequestCaptor: ArgumentCaptor[ImportRestApiRequest] = ArgumentCaptor.forClass(classOf[ImportRestApiRequest])
+      when(mockAPIGatewayClient.importRestApi(importRestApiRequestCaptor.capture())).thenReturn(apiGatewayResponse)
+
+      val result: Either[Nothing, APIGatewayProxyResponseEvent] = addApiHandler.handle(new APIGatewayProxyRequestEvent().withBody(inputBody), mock[Context])
+
+      val capturedRequest = importRestApiRequestCaptor.getValue
+      capturedRequest.body().asUtf8String() shouldEqual inputBody
+    }
+
+    "correctly handle UnauthorizedException thrown by AWS SDK" in new Setup {
+      val errorMessage = "You're an idiot"
+      when(mockAPIGatewayClient.importRestApi(any[ImportRestApiRequest])).thenThrow(UnauthorizedException.builder().message(errorMessage).build())
+
+      val result: Either[Nothing, APIGatewayProxyResponseEvent] = addApiHandler.handle(new APIGatewayProxyRequestEvent().withBody("{}"), mock[Context])
+
+      result.isRight shouldBe true
+      val Right(responseEvent) = result
+      responseEvent.getStatusCode shouldEqual HTTP_INTERNAL_ERROR
+      responseEvent.getBody shouldEqual errorMessage
+    }
   }
 }
