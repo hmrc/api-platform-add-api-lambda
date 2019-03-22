e1cebdf9425a81df3f63050403f97900d9684642
Luis Burgos <1550179+lburgos@users.noreply.github.com>
2019-03-22 11:16:37 +0000

    API-3730: changed the API gateway events to case classes for the serialisation to work properly

--- src/main/scala/uk/gov/hmrc/apiplatform/addapi/AddApiHandler.scala
+++ src/main/scala/uk/gov/hmrc/apiplatform/addapi/AddApiHandler.scala
@@ -3,10 +3,9 @@ package uk.gov.hmrc.apiplatform.addapi
 import java.net.HttpURLConnection.{HTTP_INTERNAL_ERROR, HTTP_OK}
 
 import com.amazonaws.services.lambda.runtime.{Context, LambdaLogger}
-import com.amazonaws.services.lambda.runtime.events.{APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent}
+import io.circe.generic.auto._
 import io.github.mkotsur.aws.handler.Lambda
 import io.github.mkotsur.aws.handler.Lambda._
-import io.circe.generic.auto._
 import software.amazon.awssdk.core.SdkBytes
 import software.amazon.awssdk.services.apigateway.ApiGatewayClient
 import software.amazon.awssdk.services.apigateway.model.ImportRestApiRequest
@@ -14,23 +13,21 @@ import software.amazon.awssdk.services.apigateway.model.ImportRestApiRequest
 import scala.util.{Failure, Success, Try}
 
 
-class AddApiHandler(apiGatewayClient: ApiGatewayClient) extends Lambda[APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent] {
+class AddApiHandler(apiGatewayClient: ApiGatewayClient) extends Lambda[APIGatewayRequestEvent, APIGatewayResponseEvent] {
 
   def this() {
     this(ApiGatewayClient.create())
   }
 
-  override def handle(input: APIGatewayProxyRequestEvent, context: Context): Either[Nothing, APIGatewayProxyResponseEvent] = {
+  override def handle(input: APIGatewayRequestEvent, context: Context): Either[Nothing, APIGatewayResponseEvent] = {
     val logger: LambdaLogger = context.getLogger
     logger.log(s"Input: $input")
     val importApiRequest = ImportRestApiRequest.builder().body(SdkBytes.fromUtf8String(input.getBody)).build()
     val apiGatewayResponse = Try(apiGatewayClient.importRestApi(importApiRequest))
 
     apiGatewayResponse match {
-      case Success(response) => Right(new APIGatewayProxyResponseEvent().withStatusCode(HTTP_OK).withBody(response.id()))
-      case Failure(exception) => Right(new APIGatewayProxyResponseEvent().withStatusCode(HTTP_INTERNAL_ERROR).withBody(exception.getMessage))
+      case Success(response) => Right(APIGatewayResponseEvent().withStatusCode(HTTP_OK).withBody(response.id()))
+      case Failure(exception) => Right(APIGatewayResponseEvent().withStatusCode(HTTP_INTERNAL_ERROR).withBody(exception.getMessage))
     }
-
-
   }
 }
